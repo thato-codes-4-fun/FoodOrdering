dede8f359b089616947fcbae5fa99b41e3846cc1
Thato Lebethe <thatofreelance@gmail.com>
2024-02-07 15:02:58 +0200

    first ui component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,51 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import products from '../../../assets/data/products';
+import { Image } from 'expo-image';
+import Colors from '@/constants/Colors';
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      <Image
+        style={styles.image}
+        source={product.image}
+        placeholder='pizza image'
+        contentFit="contain"
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price.toFixed(2)}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 20
+  },
+  image: {
+    width: '100%',
+  
+    alignSelf: 'center',
+    aspectRatio: 1,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical: 10,
+
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  price: {
+    marginTop: 'auto',
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  }
 });
+
+
